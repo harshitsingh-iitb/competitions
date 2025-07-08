e2690a75fbcd0557edefd81da7e08c10f92919d9
Harry-0318 <harshitbhandari0318@gmail.com>
2025-07-08 17:00:08 +0530

    Backend Connected

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // App.jsx
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, data } from 'react-router-dom';
 import Home from './pages/Home';
 import CompPage from './pages/compPage';
 import ScrollToTop from "./components/toTop"; 
@@ -10,6 +10,14 @@ import NotFound from './pages/notFound';
 
 const isLive = false; 
 function App() {
+  // fetch("https://my-backend.onrender.com/status")
+fetch("https://comp-0al6.onrender.com/status")
+  .then(response => response.json())
+  .then(data => {
+    console.log(data); 
+  });
+  const isLive = data.isLive;
+  console.log("Is live status:", isLive); 
   return (
     <Router>
       <ScrollToTop />
